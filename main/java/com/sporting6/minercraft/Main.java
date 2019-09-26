7c12b0c49058956ecd838059da559bb37c10ef42
Teddy Maloney <theodore.p.maloney@gmail.com>
2019-09-25 19:21:10 -0500

    just starting new version

---
+++ main/java/com/sporting6/minercraft/Main.java
@@ -0,0 +1,13 @@
+ package com.sporting6.minercraft;
+
+import com.sporting6.minercraft.util.Reference;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.Instance; 
+
+@Mod(modid = Reference.MOD_ID,name =Reference.NAME, version = Reference.VERSION )
+public class Main {
+
+	@Instance
+	public static Main instance;
+}
---
+++ main/java/com/sporting6/minercraft/util/Reference.java
@@ -0,0 +1,15 @@
+package com.sporting6.minercraft.util;
+
+public class Reference {
+
+	public static final String MOD_ID = "mc";
+	public static final String NAME = "Miner Craft ";
+	public static final String VERSION = "1.0.0 ";
+	public static final String ACCEPED_VERSIONS = "[1.12.2] ";
+	public static final String CLIENT_PROXY_CLASS = "";
+	public static final String COMMON_PROXY_CLASS = "";
+
+	
+
+
+}
