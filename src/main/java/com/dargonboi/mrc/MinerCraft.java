196f9326da2ba0bc6e484d9f26cdad37d0a829a1
Jack Maloney <theodore.p.maloney@gmail.com>
2022-01-17 21:33:45 -0600

    Eclipse works

---
+++ src/main/java/com/dargonboi/mrc/MinerCraft.java
@@ -0,0 +1,12 @@
+package com.dargonboi.mrc;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(MinerCraft.MODID)
+public class MinerCraft {
+	public static final String MODID = "mrc";
+	
+	public MinerCraft() {
+		
+	}
+}
