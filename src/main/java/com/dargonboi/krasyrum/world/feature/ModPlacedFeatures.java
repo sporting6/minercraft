2c846c376e1c7d66227b59634749e4b932587e4c
sporting6 <54516771+sporting6@users.noreply.github.com>
2023-02-06 14:50:09 -0600

    Update ModPlacedFeatures.java

--- src/main/java/com/dargonboi/krasyrum/world/feature/ModPlacedFeatures.java
+++ src/main/java/com/dargonboi/krasyrum/world/feature/ModPlacedFeatures.java
@@ -31,38 +31,15 @@ public class ModPlacedFeatures {
 
         PLACEDOREMAP.put(name, ORE_PLACED);
     }
-            
-
-    //Overworld Ores-------------------------------------------------------------------------------------------------------------------------------------------------------------
-    public static final RegistryObject<PlacedFeature> BLUE_NANITARIUM_ORE_PLACED = PLACED_FEATURES.register("blue_nanitarium_ore_placed",
-            () -> new PlacedFeature(ModConfiguredFeatures.BLUE_NANITARIUM_ORE.getHolder().get(), ModOrePlacement.rareOrePlacement(1, //    Veins Per Chunk
-                    HeightRangePlacement.triangle(VerticalAnchor.bottom(), VerticalAnchor.aboveBottom(32)))));
-
-//    public static final RegistryObject<PlacedFeature> TITANIUM_ORE_PLACED = PLACED_FEATURES.register("titanium_ore_placed",
-//            () -> new PlacedFeature(ModConfiguredFeatures.OREMAP.get("titanium_ore").getHolder().get(),
-//                    ModOrePlacement.commonOrePlacement(14, //    Veins Per Chunk
-//                    HeightRangePlacement.triangle(VerticalAnchor.bottom(), VerticalAnchor.aboveBottom(72)))));
-
-    public static final RegistryObject<PlacedFeature> RUBY_ORE_PLACED = PLACED_FEATURES.register("ruby_ore_placed",
-            () -> new PlacedFeature(ModConfiguredFeatures.RUBY_ORE.getHolder().get(), ModOrePlacement.commonOrePlacement(12, //    Veins Per Chunk
-                    HeightRangePlacement.triangle(VerticalAnchor.bottom(), VerticalAnchor.aboveBottom(72)))));
-    //Nether Ores-------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-    public static final RegistryObject<PlacedFeature> NETHER_RED_NANITARIUM_ORE_PLACED = PLACED_FEATURES.register("nether_red_nanitarium_ore_placed",
-            () -> new PlacedFeature(ModConfiguredFeatures.RED_NANITARIUM_ORE.getHolder().get(), ModOrePlacement.commonOrePlacement(1, // VeinsPerChunk
-                    HeightRangePlacement.uniform(VerticalAnchor.aboveBottom(90), VerticalAnchor.top()))));
-    //End Ores-------------------------------------------------------------------------------------------------------------------------------------------------------------
-    public static final RegistryObject<PlacedFeature> THERIUM_ORE_PLACED = PLACED_FEATURES.register("veranium_ore_placed",
-            () -> new PlacedFeature(ModConfiguredFeatures.THERIUM_ORE.getHolder().get(), ModOrePlacement.rareOrePlacement(7, //    Veins Per Chunk
-                    HeightRangePlacement.triangle(VerticalAnchor.bottom(), VerticalAnchor.aboveBottom(100)))));
-
-    public static final RegistryObject<PlacedFeature> VERANIUM_ORE_PLACED = PLACED_FEATURES.register("therium_ore_placed",
-            () -> new PlacedFeature(ModConfiguredFeatures.VERANIUM_ORE.getHolder().get(), ModOrePlacement.rareOrePlacement(5, //    Veins Per Chunk
-                    HeightRangePlacement.triangle(VerticalAnchor.bottom(), VerticalAnchor.aboveBottom(75)))));
-
+    
     public static void register(IEventBus eventBus) {
         placeOres("titanium_ore", ModConfiguredFeatures.OREMAP, 14, -64, 16);
-
+        placeOres("blue_nanitarium_ore", ModConfiguredFeatures.OREMAP, 1, -64, -32);
+        placeOres("red_nanitarium_ore", ModConfiguredFeatures.OREMAP, 1, 90, 125);
+        placeOres("ruby_ore", ModConfiguredFeatures.OREMAP, 12, -64, 16);
+        placeOres("veranium_ore", ModConfiguredFeatures.OREMAP, 5, 0, 100);
+        placeOres("therium_ore", ModConfiguredFeatures.OREMAP, 5, 0, 100);
+        
         PLACED_FEATURES.register(eventBus);
     }
 
