bdfe8fef92710400e078eb48783b741097a976e0
Dargonboi <theodore.p.maloney@gmail.com>
2023-03-16 12:21:34 -0500

    Finally Got Lava Pools Working

---
+++ src/main/java/com/dargonboi/krasyrum/util/world/feature/FeatureHelper.java
@@ -0,0 +1,94 @@
+package com.dargonboi.krasyrum.util.world.feature;
+
+import com.dargonboi.krasyrum.block.ModBlocks;
+import com.dargonboi.krasyrum.util.Material;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.WorldGenLevel;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockState;
+
+public class FeatureHelper {
+
+
+    public static void circle(BlockPos origin, Block block, int radius, WorldGenLevel level) {
+        ellipse(origin, block, radius, radius, level);
+    }
+
+    public static void filledEllipse(BlockPos origin, Block block, double a, double b, WorldGenLevel level){
+        level.setBlock(origin, block.defaultBlockState(), 2);
+        for(int i = 0; i <= a; i++){
+            for(int j = 0; j <= b; j++) {
+                ellipse(origin, block, i, j, level);
+            }
+        }
+    }
+
+    public static void filledNoOverhangEllipse(BlockPos origin, Block block, double a, double b, WorldGenLevel level){
+        BlockState originalBlock = level.getBlockState(origin);
+        level.setBlock(origin, block.defaultBlockState(), 2);
+        for(int i = 0; i <= a; i++){
+            for(int j = 0; j <= b; j++) {
+                noOverhangEllipse(origin, block, i, j, level);
+            }
+        }
+        noOverhangEllipse(origin, originalBlock.getBlock(), a + 1, b + 1, level);
+    }
+
+
+
+    public static void noOverhangEllipse(BlockPos origin, Block block, double a, double b, WorldGenLevel level){
+        BlockState originalBlock = level.getBlockState(origin);
+        for(double x = 0 - a; x <= a; x = x + 0.25){
+            double y = (b/a) * Math.sqrt((a * a) - (x * x));
+//            System.out.println("X = " + x + ", Y = " + y);
+
+
+            BlockPos pos = origin.east((int)x).north((int)y);
+            if (!blockCheck(pos, level, 1, Blocks.AIR.defaultBlockState())
+                    && !blockCheck(pos, level, 1, Blocks.CAVE_AIR.defaultBlockState())
+                    && !blockCheck(pos, level, 1, Blocks.WATER.defaultBlockState())
+                    && !blockCheck(pos, level, 1, ModBlocks.BOILING_WATER_BLOCK.get().defaultBlockState())) {
+                level.setBlock(pos, block.defaultBlockState(), 2);
+            }
+
+            pos = origin.west((int)x).south((int)y);
+            if (!blockCheck(pos, level, 1, Blocks.AIR.defaultBlockState())
+                    && !blockCheck(pos, level, 1, Blocks.CAVE_AIR.defaultBlockState())
+                    && !blockCheck(pos, level, 1, Blocks.WATER.defaultBlockState())
+                    && !blockCheck(pos, level, 1, ModBlocks.BOILING_WATER_BLOCK.get().defaultBlockState())) {
+                level.setBlock(pos, block.defaultBlockState(), 2);
+            }
+        }
+//        level.setBlock(origin.west((int)a), originalBlock, 2);
+//        level.setBlock(origin.east((int)a), originalBlock, 2);
+//        level.setBlock(origin.north((int)b), originalBlock, 2);
+//        level.setBlock(origin.south((int)b), originalBlock, 2);
+    }
+    public static void ellipse(BlockPos origin, Block block, double a, double b, WorldGenLevel level){
+        BlockState originalBlock = level.getBlockState(origin);
+        for(double x = 0 - a; x <= a; x = x + 0.25){
+            double y = (b/a) * Math.sqrt((a * a) - (x * x));
+//            System.out.println("X = " + x + ", Y = " + y);
+            level.setBlock(origin.east((int)x).north((int)y), block.defaultBlockState(), 2);
+            level.setBlock(origin.west((int)x).south((int)y), block.defaultBlockState(), 2);
+        }
+        level.setBlock(origin.west((int)a), originalBlock, 2);
+        level.setBlock(origin.east((int)a), originalBlock, 2);
+        level.setBlock(origin.north((int)b), originalBlock, 2);
+        level.setBlock(origin.south((int)b), originalBlock, 2);
+    }
+
+    public static boolean blockCheck(BlockPos pos, WorldGenLevel level, int radius, BlockState blockState){
+        BlockPos origin = pos.west(radius).north(radius);;
+//        System.out.println("Checking For " + blockState + " In a Radius of " + radius);
+        for(int i = 0; i <= radius * 2; i ++){
+            for(int j = 0; j <= radius * 2; j ++){
+                if(level.getBlockState(origin.south(i).east(j)) == blockState){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
--- src/main/java/com/dargonboi/krasyrum/world/feature/ore/LavaPoolFeature.java
+++ src/main/java/com/dargonboi/krasyrum/world/feature/ore/LavaPoolFeature.java
@@ -1,13 +1,20 @@
 package com.dargonboi.krasyrum.world.feature.ore;
 
+import com.dargonboi.krasyrum.Krasyrum;
+import com.dargonboi.krasyrum.block.ModBlocks;
+import com.dargonboi.krasyrum.util.world.feature.FeatureHelper;
 import com.mojang.serialization.Codec;
 import net.minecraft.core.BlockPos;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.WorldGenLevel;
+import net.minecraft.world.level.block.AirBlock;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.levelgen.feature.Feature;
 import net.minecraft.world.level.levelgen.feature.FeaturePlaceContext;
 import net.minecraft.world.level.levelgen.feature.configurations.NoneFeatureConfiguration;
+
+import java.security.Key;
 
 public class LavaPoolFeature extends Feature<NoneFeatureConfiguration> {
 
@@ -21,44 +28,47 @@ public class LavaPoolFeature extends Feature<NoneFeatureConfiguration> {
 
     @Override
     public boolean place(FeaturePlaceContext<NoneFeatureConfiguration> pContext) {
-        BlockPos pos = pContext.origin();
+        BlockPos origin = pContext.origin().below();
         WorldGenLevel worldgenlevel = pContext.level();
         RandomSource randomsource = pContext.random();
-        if(!worldgenlevel.isEmptyBlock(pos) && !worldgenlevel.isEmptyBlock(pos.below(1))){
-            worldgenlevel.setBlock(pos, Blocks.LAVA.defaultBlockState(), 2);
-
-            int northMax = randomsource.nextInt(poolMaxRadius + 1);
-            int southMax = randomsource.nextInt(poolMaxRadius + 1);
-            int eastMax = randomsource.nextInt(poolMaxRadius + 1);
-            int westMax = randomsource.nextInt(poolMaxRadius + 1);
-
-
-            for(int i = -southMax; i < northMax; i++){
-                for(int j = -westMax; i < eastMax; j++){
-                    if(i < 0 && j < 0){
-                        worldgenlevel.setBlock(pos.south(Math.abs(i)).east(Math.abs(j)), Blocks.LAVA.defaultBlockState(), 2);
-                    }
-                    if(i < 0 && j > 0){
-                        worldgenlevel.setBlock(pos.south(Math.abs(i)).west(Math.abs(j)), Blocks.LAVA.defaultBlockState(), 2);
-                    }
-                    if(i > 0 && j < 0){
-                        worldgenlevel.setBlock(pos.north(Math.abs(i)).east(Math.abs(j)), Blocks.LAVA.defaultBlockState(), 2);
-                    }
-                    if(i > 0 && j > 0){
-                        worldgenlevel.setBlock(pos.north(Math.abs(i)).west(Math.abs(j)), Blocks.LAVA.defaultBlockState(), 2);
-                    }
-                }
-            }
-            System.out.println(northMax);
-            System.out.println(southMax);
-            System.out.println(eastMax);
-            System.out.println(westMax);
-
-            return true;
+        if(worldgenlevel.getBlockState(origin.below()) == ModBlocks.BOILING_WATER_BLOCK.get().defaultBlockState()){
+            return false;
+        }
+        if(worldgenlevel.getBlockState(origin.above()) != Blocks.AIR.defaultBlockState()){
+            return false;
         }
 
-        System.out.println("===================> Lava Pool Registration Failed");
-        return false;
+        if(FeatureHelper.blockCheck(origin, worldgenlevel, 2, Blocks.AIR.defaultBlockState()))
+            return false;
+        if(FeatureHelper.blockCheck(origin, worldgenlevel, 2, Blocks.CAVE_AIR.defaultBlockState()))
+            return false;
+
+
+        int w = randomsource.nextInt(6);
+        int h = randomsource.nextInt(6);
+        FeatureHelper.filledNoOverhangEllipse(origin, Blocks.LAVA, w, h, worldgenlevel);
+        int poolCount = randomsource.nextInt(3);
+        for(int i = 0; i <= poolCount; i++) {
+
+            int north = randomsource.nextInt(12) - 6;
+            int west = randomsource.nextInt(12) - 6;
+
+            if(north < 2 && north > -2)
+                north = north + 3;
+            if(west < 2 && west > -2)
+                west = west + 3;
+
+            int a = randomsource.nextInt(4) + 2;
+            int b = randomsource.nextInt(4) + 2;
+
+            FeatureHelper.filledNoOverhangEllipse(origin.north(north).west(west), Blocks.LAVA, randomsource.nextInt(6), randomsource.nextInt(6), worldgenlevel);
+        }
+
+
+
+
+        System.out.println("===================> Lava Pool Registration Success");
+        return true;
     }
 
 }
